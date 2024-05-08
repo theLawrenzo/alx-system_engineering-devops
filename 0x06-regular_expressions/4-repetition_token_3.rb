bf6394a5f6d52b90f62c77b2307308b33b49dce8
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-05-08 16:16:38 -0700

    RUBY SCRIPT [4-repetition_token_3.rb] - Regular expression

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,2 +1,19 @@
 #!/usr/bin/env ruby
+# Script that accepts one argument and pass it to a regular expression
+# matching method
 
+# Check if command line argument is empty then exit
+if ARGV.empty?
+  exit
+end
+
+# Assign first command line argument to a variable
+word = ARGV[0]
+
+# Regex pattern to search for word
+pattern = /hbt?{1,4}n/i
+
+# Compare Regex with word if they match tne output word
+if word =~ pattern
+  puts word
+end

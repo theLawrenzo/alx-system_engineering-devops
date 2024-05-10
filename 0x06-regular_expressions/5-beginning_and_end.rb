24144515a95916c7e2bb19e2f0f83f4c8e97a4c5
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-05-10 15:57:07 -0700

    RUBY SCRIPT [5-beginning_and_end.rb] - Regex to check words that begins with h and ends with n, with just a single character in between

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+# This script contains regular expression that matches a word that
+# begins with a 'h' and ends with an 'n' and has any single character
+# in-between
+
+# Check if command line arguments is empty
+if ARGV.empty?
+  exit
+end
+
+# Initialize a variable with the first command line argument
+word = ARGV[0]
+
+# Regex pattern to check for match
+pattern = /^h[a-zA-Z0-9]$n/i
+
+# Use pattern to check cmd argument if they're a match
+if word =~ pattern
+  print word
+end

4715c72be61a0f11ef54950b88f931abcf3a232f
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-05-10 21:48:30 -0700

    RUBY SCRIPT

---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+# This Script contains Regex that only matches uppercase letters
+
+if ARGV.empty?
+  exit
+end
+
+word = ARGV[0]
+
+regex = /([A-Z]+ | "[A-Z ]+"")/i
+
+matches = word.scan(regex);
+
+if matches.empty?
+  exit
+else
+  matches.each do |match|
+    puts match
+  end
+end
